7becfa24d55166a13cd1b03d4936f83027ab41f8
surprise <viktar.yeustsihneyeu@instinctools.com>
2022-12-25 03:22:36 +0300

    init

---
+++ src/models/People.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+const PeopleSchema = new Schema({
+	name: {
+		type: String,
+	},
+	avatar: {
+		type: String,
+		default: 'default.png',
+	},
+	typeThing: {
+		type: String,
+	},
+	color: {
+		type: String,
+	},
+	isBar: {
+		type: Boolean,
+	},
+});
+
+const People = model('People', PeopleSchema);
+
+const createTeam = (data) => {
+	return People.create(data);
+};
+
+const getTeamData = () => {
+	return People.find();
+};
+
+const getPersonData = (id) => {
+	return People.findOne({ _id: id });
+};
+
+const deleteTeam = (id) => {
+	return People.deleteOne({ _id: id });
+};
+
+const updateTeam = (id, data) => {
+	return People.updateOne({ _id: id }, { ...data });
+};
+
+module.exports = {
+	createTeam,
+	getTeamData,
+	deleteTeam,
+	updateTeam,
+	getPersonData,
+	People,
+};
